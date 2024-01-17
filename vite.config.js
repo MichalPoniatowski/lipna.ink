c2395b6374fc07aac47b6df3499ccfac8976d5a9
MichalPoniatowski <m.poniatowski87@gmail.com>
2024-01-17 19:41:32 +0100

    vite.config and package.json correced

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/lipna.ink",
   plugins: [react()],
   assetsInclude: ["**/*.JPG", "**/*.jpg"],
 });
