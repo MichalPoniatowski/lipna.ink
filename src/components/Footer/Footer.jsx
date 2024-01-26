c1b3ee1c629f7f5415d076f757b6d5d183ccc7a0
MichalPoniatowski <m.poniatowski87@gmail.com>
2024-01-26 13:00:34 +0100

    active adress and phone added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,12 +12,15 @@ export const Footer = () => {
           <br />
           studio
         </p>
-        <address>
-          ul. Wandy14/16
-          <br />
-          53 320 Wrocław
-        </address>
-        <a href="+48777888666">tel: 777 888 666</a>
+        <a href="https://maps.app.goo.gl/jLiNrDgLBMNMR2wE9" target="_blank">
+          <address>
+            ul. Wandy14/16
+            <br />
+            53 320 Wrocław
+          </address>
+        </a>
+
+        <a href="tel:+48777888666">tel: 777 888 666</a>
       </div>
       <div className={css.footerSocial}>
         <SocialMedia />
