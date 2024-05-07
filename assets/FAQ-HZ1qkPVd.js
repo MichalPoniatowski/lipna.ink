2d759540116df1c40f75776d0a2be58494718953
MichalPoniatowski <118760083+MichalPoniatowski@users.noreply.github.com>
2024-05-07 18:04:29 +0000

    Deploying to gh-pages from @ MichalPoniatowski/lipna.ink@faf6a14a4986b9e235c2965d0ec67a6ff5bc0408 🚀

---
+++ assets/FAQ-HZ1qkPVd.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-fBoJnhct.js";const t=()=>r.jsx("div",{children:r.jsx("h2",{children:"PYTANIA I ODPOWIEDZI"})});export{t as default};
--- assets/NotFound-9nEKpA28.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-H01rAAnE.js";const p="_wrapper_1lgfk_1",s={wrapper:p},o=()=>r.jsx("main",{className:s.wrapper,children:r.jsx("p",{children:"Sorry, page not found :("})});export{o as default};
