02e54626ddb43f57e6897012b416e52a673cd9a6
MichalPoniatowski <118760083+MichalPoniatowski@users.noreply.github.com>
2024-05-09 13:28:05 +0000

    Deploying to gh-pages from @ MichalPoniatowski/lipna.ink@f9f75d7e7373ac569b95aba1bff7937becdbbf6b 🚀

--- assets/NotFound-ZL99970g.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-tp_Y9nTr.js";const p="_wrapper_1lgfk_1",s={wrapper:p},o=()=>r.jsx("main",{className:s.wrapper,children:r.jsx("p",{children:"Sorry, page not found :("})});export{o as default};
